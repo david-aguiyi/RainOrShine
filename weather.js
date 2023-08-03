77228782ceb086efa9d72e8b22f4de824602297b
david-aguiyi <davidaguiyi9@gmail.com>
2023-08-03 13:42:03 +0100

    Mapping custom icon

--- weather.js
+++ weather.js
@@ -1,30 +1,3 @@
-// let weather = {
-// 	apiKey: "npn0ofydsxc70vmz60bg10uxosy5arpar03ami77",
-// 	fetchWeather: function (city) {
-// 		fetch(
-// 			`https://www.meteosource.com/api/v1/free/point?place_id=${city}&sections=all&timezone=UTC&language=en&units=metric&key=${this.apiKey}`
-// 		)
-// 			.then((response) => {
-// 				if (!response.ok) {
-// 					throw new Error("Network response was not ok");
-// 				}
-// 				return response.json();
-// 			})
-// 			.then((data) => {
-//         this.displayWeather(data);
-// 			});
-// 	},
-
-// 	displayWeather: function (data) {
-//     const { icon, summary, temperature } = data.hourly.data[2];
-//     const forecastList = data.daily.data;
-//     const date = new Date(forecastList[0].day);
-//     const options = { weekday: 'short', day: 'numeric', month: 'short' };
-//     const formattedDate = date.toLocaleDateString(undefined, options);
-//     console.log(icon, summary, temperature, formattedDate);
-// 	},
-// };
-
 let weather = {
 	forecastApiKey: "npn0ofydsxc70vmz60bg10uxosy5arpar03ami77",
 	currentApiKey: "420e99cc4adf023e23735dcf8cb3512c",
@@ -46,26 +19,58 @@ let weather = {
 	},
 
 	displayCurrentWeather: function (data) {
+
+		//custom-icon mapping
+		const iconMapping = {
+			"01d": "Clear.png",
+			"01n": "Clear.png",
+			"02d": "HeavyCloud.png",
+			"02n": "HeavyCloud.png",
+			"03d": "LightCloud.png",
+			"03n": "LightCloud.png",
+			"04d": "overcast.png",
+			"04n": "overcast.png",
+			"09d": "HeavyRain.png",
+			"09n": "HeavyRain.png",
+			"10d": "LightRain.png",
+			"10n": "LightRain.png",
+			"11d": "Thunderstorm.png",
+			"11n": "Thunderstorm.png",
+			"13d": "Snow.png",
+			"13n": "Sleet.png",
+		};
+
 		const { name } = data;
 		const { icon, description } = data.weather[0];
-		const { temp, humidity, pressure} = data.main;
+		const { temp, humidity, pressure } = data.main;
 		const { speed } = data.wind;
 		const { visibility } = data;
-		
+
 		var miles = Math.round((visibility / 1609.344) * 10) / 10;
 		var ctemp = Math.floor(temp - 273.15);
-		console.log(name, icon, description, ctemp, humidity, speed, pressure, miles);
+		console.log(
+			name,
+			icon,
+			description,
+			ctemp,
+			humidity,
+			speed,
+			pressure,
+			miles
+		);
 
 		document.querySelector(".weather-location span").innerText = name;
-		document.querySelector("#weather-icon").src =
-			"https://openweathermap.org/img/wn/" + icon + ".png";
 		document.querySelector(".weather-info__temp p").innerText = description;
 		document.querySelector(".weather-info__temp h1").innerText = ctemp;
 		document.querySelector(".humidity").innerText = humidity;
 		document.querySelector(".progress-yellow").style.width = humidity + "%";
 		document.querySelector(".wind").innerText = speed;
-		document.querySelector('.pressure').innerText = pressure;
+		document.querySelector(".pressure").innerText = pressure;
 		document.querySelector(".visibility").innerText = miles;
+		
+		//custom icon
+		const iconElement = document.querySelector("#weather-icon");
+		iconElement.src = `images/${iconMapping[icon]}`;
 	},
 
 	displayForecastWeather: function (data2) {
